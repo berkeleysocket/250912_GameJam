b4c60a23d759dcaec0076e3a4ef00c233ca2860b
oshe <shinyjune11@gmail.com>
2025-12-21 23:33:29 +0900

    feat : none

--- Assets/AJ/01.Scripts/Director.cs
+++ Assets/AJ/01.Scripts/Director.cs
@@ -1,9 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace AJ._01.Scripts
 {
     public class Director : MonoBehaviour
     {
-        
+        [field:SerializeField] public Transform Target { get; private set; }
+        [field:SerializeField] public NavMeshAgent AgentCompo { get; private set; }
+
+        [SerializeField] private bool canMove = true;
+        [SerializeField] private float speed;
+        public float Speed
+        {
+            get => speed;
+            set
+            {
+                speed = value;
+                AgentCompo.speed = speed;
+            }
+        }
+
+        private void Awake()
+        {
+            AgentCompo.speed = speed;
+            AgentCompo.updateRotation = false;
+            AgentCompo.updateUpAxis = false;
+        }
+
+        private void Update()
+        {
+            if (canMove)
+                AgentCompo.SetDestination(Target.position);
+        }
     }
 }
