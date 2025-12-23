6d3ec9db7849677f5115b53520ca52ec78127016
ageuha <108604569+ageuha@users.noreply.github.com>
2025-12-23 22:44:17 +0900

    Update Player Prefab

--- Assets/_Scripts/KSY/Player/Player.cs
+++ Assets/_Scripts/KSY/Player/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 using _Scripts.Core.Input;
 using Ksy.Scripts.StressSystem;
+using _Scripts.Core.Events;
 
 namespace Ksy.Scripts._Player
 {
@@ -12,6 +13,7 @@ namespace Ksy.Scripts._Player
         [field: SerializeField] public RendererX RednererCompo {get; private set;}
         [field: SerializeField] public AnimationX AnimationCompo {get; private set;}
         [field: SerializeField] public AudioSource AudioSourceCompo {get; private set;}
+        [SerializeField] private EmptyEventChannel gameOverEventChannel;
 
         void Start()
         {
@@ -48,6 +50,14 @@ namespace Ksy.Scripts._Player
             {
                 MovementCompo.notify_IsMove.OnChangedValue -= AnimationCompo.SetIsMove;
                 MovementCompo.notify_Dir.OnChangedValue -= AnimationCompo.SetMoveDir;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) 
+        {
+            if (other.CompareTag("Director"))
+            {
+                gameOverEventChannel.Raise(new());
             }
         }
 
